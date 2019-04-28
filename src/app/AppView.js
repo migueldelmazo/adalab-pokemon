47fd52479a37ffa3fb171cf868100667ff17cd23
Miguel del Mazo <info@migueldelmazo.com>
2019-04-28 09:40:21 +0200

    Added reset, main and layout styles

--- src/app/AppView.js
+++ src/app/AppView.js
@@ -2,14 +2,20 @@ import React from 'react'
 import Component from '../libs/react'
 import ItemsView from './ItemsView'
 import SearchView from './SearchView'
+import './Main.scss'
+import './App.scss'
 
 export default class AppView extends Component {
 
   render() {
     return (
-      <div>
+      <div className='layout'>
         <SearchView />
         <ItemsView />
+        <div className='corner corner--top corner--left' />
+        <div className='corner corner--top corner--right' />
+        <div className='corner corner--bottom corner--left' />
+        <div className='corner corner--bottom corner--right' />
       </div>
     )
   }
